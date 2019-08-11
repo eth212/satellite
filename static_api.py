d50d32d5649624e16de5c22a0e3cf05d1aadc539
eth212 <ethanbonnardeaux@gmail.com>
2019-08-10 20:44:41 -0400

    adding api image GET script

--- static_api.py
+++ static_api.py
@@ -3,11 +3,19 @@ import cv2
 import shutil
 import pandas as pd
 
-data = pd.read_csv("")
-
-
-response = requests.get("https://maps.googleapis.com/maps/api/staticmap?center=47.5704045,-53.274431&zoom=13&size=267x341&scale=2&maptype=satellite&key=AIzaSyDYWsrS6XuUJ-58ewStqDiBsqDQHHRTnbM", stream=True)
-
-with open('images/img.png', 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
-del response
+buffer = pd.read_csv("training1.csv")
+data = pd.DataFrame(buffer)
+data = data.drop_duplicates(subset=["centroid_lat", "centroid_long", "Zoom", "width", "height"],keep="first")
+print(data)
+for i in range(0,100000):
+    PostCode = str(data.iloc[i, 0])
+    centroid_lat = str(data.iloc[i, 5])
+    centroid_long = str(data.iloc[i, 6])
+    zoom = str(data.iloc[i, 9])
+    height = str(data.iloc[i, 10])
+    width = str(data.iloc[i, 11])
+    query = "https://maps.googleapis.com/maps/api/staticmap?center=" + centroid_lat  + ","  + centroid_long + "&zoom=" + zoom + "&size=" + width + "x" + height + "&scale=2&maptype=satellite&key=AIzaSyDYWsrS6XuUJ-58ewStqDiBsqDQHHRTnbM"
+    response = requests.get(query, stream=True)
+    with open("images/" + PostCode + ".png", 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    del response
