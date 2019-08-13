260521550532cd8c9b99efed8a1391602d971a2f
eth212 <ethanbonnardeaux@gmail.com>
2019-08-13 10:46:57 -0400

    api script update

--- static_api.py
+++ static_api.py
@@ -3,25 +3,45 @@ import cv2
 import shutil
 import pandas as pd
 import numpy as np
+import sys
+import time
 
-headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+proxies = {
+"http": "http://ip:port",
+"https": "http://ip:port",
+}
+
 
 buffer = pd.read_csv("training1.csv")
 data = pd.DataFrame(buffer)
-data = data.drop_duplicates(subset=["centroid_lat", "centroid_long", "Zoom", "width", "height"],keep="first")
-print(data[data['PostCode']=='B1E1H4'])
-amount = np.linspace(0,7771, 7771)
-data.iloc[0:12446, 1].nunique()
+last_index = data[data['PostCode']=='E3B9E2'].index.values.astype(int)[0]
+amount = np.linspace(0,77, 7771)
+position = data.iloc[0:last_index, 1].nunique()
+print(position)
 
-for i in range(10305,100000):
-    PostCode = str(data.iloc[i, 0])
-    centroid_lat = str(data.iloc[i, 5])
-    centroid_long = str(data.iloc[i, 6])
-    zoom = str(data.iloc[i, 9])
-    height = str(data.iloc[i, 10])
-    width = str(data.iloc[i, 11])
-    query = "https://maps.googleapis.com/maps/api/staticmap?center=" + centroid_lat  + ","  + centroid_long + "&zoom=" + zoom + "&size=" + width + "x" + height + "&scale=2&maptype=satellite&key=AIzaSyDYWsrS6XuUJ-58ewStqDiBsqDQHHRTnbM"
-    response = requests.get(query, headers=headers, stream=True, timeout=5)
-    with open("images/" + PostCode + ".png", 'wb') as out_file:
-        shutil.copyfileobj(response.raw, out_file)
-    del response
+data = data.drop_duplicates(subset=["centroid_lat", "centroid_long", "Zoom", "width", "height"],keep="first")
+target = 900000
+def runApi(current_position, target):
+
+    for i in range(current_position + 100 ,target):
+        PostCode = str(data.iloc[i, 0])
+        centroid_lat = str(data.iloc[i, 5])
+        centroid_long = str(data.iloc[i, 6])
+        zoom = str(data.iloc[i, 9])
+        height = str(data.iloc[i, 10])
+        width = str(data.iloc[i, 11])
+        query = "https://maps.googleapis.com/maps/api/staticmap?center=" + centroid_lat  + ","  + centroid_long + "&zoom=" + zoom + "&size=" + width + "x" + height + "&scale=2&maptype=satellite&key=AIzaSyDYWsrS6XuUJ-58ewStqDiBsqDQHHRTnbM"
+        try:
+            response = requests.get(query, headers=headers, stream=True)
+            with open("images/" + PostCode + ".png", 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            del response
+
+        except requests.ConnectionError as e:
+            print(e)
+            time.sleep(60)
+            print("\n the program is sleeping\n")
+            runApi(i, target)
+
+runApi(position, target)
